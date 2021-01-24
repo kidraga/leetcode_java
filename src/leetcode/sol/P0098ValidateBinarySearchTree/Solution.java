827c360966b0969ad638b34068d01121942d7753
Zheng Luo <kidraga@gmail.com>
2021-01-24 14:48:00 -0800

    Problem 98

---
+++ src/leetcode/sol/P0098ValidateBinarySearchTree/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.sol.P0098ValidateBinarySearchTree;
+
+import leetcode.sol.util.TreeNode;
+
+public class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+
+        return isValidBSTWithBoundaryCheck(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBSTWithBoundaryCheck(TreeNode root, long minValue, long maxValue) {
+
+        if (root == null) {
+            return true;
+        }
+
+        if (root.val <= minValue) return false;
+        if (root.val >= maxValue) return false;
+
+        return isValidBSTWithBoundaryCheck(root.left, minValue, root.val)
+                && isValidBSTWithBoundaryCheck(root.right, root.val, maxValue);
+    }
+}
