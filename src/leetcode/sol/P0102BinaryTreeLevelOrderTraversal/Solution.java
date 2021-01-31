c712752e2ba66e4c2a4609cd6af97185f379a7b1
Zheng Luo <kidraga@gmail.com>
2021-01-31 01:36:29 -0800

    Promblem 102

---
+++ src/leetcode/sol/P0102BinaryTreeLevelOrderTraversal/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.sol.P0102BinaryTreeLevelOrderTraversal;
+
+import leetcode.sol.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    /**
+     * Recursive solution
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        dfs(result, root, 0);
+        return result;
+    }
+
+    private void dfs(List<List<Integer>> result, TreeNode node, int level) {
+
+        if (node == null) return;
+        // level equals result.size() when we reach this level for the first time.
+        if (level == result.size()) result.add(new ArrayList<Integer>());
+
+        result.get(level).add(node.val);
+        dfs(result, node.left, level + 1);
+        dfs(result, node.right, level + 1);
+    }
+}
