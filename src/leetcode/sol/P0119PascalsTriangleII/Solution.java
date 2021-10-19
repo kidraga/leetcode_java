0107b47063ec4c6d8b839914c3d3fdb74dfbfc3d
Zheng Luo <kidraga@gmail.com>
2021-10-18 21:28:25 -0700

    Problem 119

---
+++ src/leetcode/sol/P0119PascalsTriangleII/Solution.java
@@ -0,0 +1,36 @@
+package leetcode.sol.P0119PascalsTriangleII;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<>();
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        // Base case; first row is always [1];
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
+
+        for (int rowNum = 1; rowNum <= rowIndex; rowNum++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> preRow = triangle.get(rowNum - 1);
+
+            // First element of each row is always 1.
+            row.add(1);
+
+            // calculate each element.
+            // each element is equal to element above and above-and-left
+            for (int j = 1; j < rowNum; j++) {
+                row.add(preRow.get(j - 1) + preRow.get(j));
+            }
+
+            // the last element is always 1.
+            row.add(1);
+
+            triangle.add(row);
+        }
+
+        return triangle.get(triangle.size() - 1);
+    }
+}
