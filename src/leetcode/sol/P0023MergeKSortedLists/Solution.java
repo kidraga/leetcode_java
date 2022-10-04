61ee3cbcc5123e3d3055c1b335f253219de32701
Zheng Luo <kidraga@gmail.com>
2022-10-04 11:50:49 -0700

    Problem 23

--- src/leetcode/sol/P0023MergeKSortedLists/Solution.java
+++ src/leetcode/sol/P0023MergeKSortedLists/Solution.java
@@ -1,4 +1,43 @@
 package leetcode.sol.P0023MergeKSortedLists;
 
+import leetcode.sol.util.ListNode;
+
+import java.util.PriorityQueue;
+
+/**
+ * Time: O(Nlogk). N total number of all nodes. k: number of links
+ */
 public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+
+        // min heap
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(
+                lists.length,
+                (a, b) -> (a.val - b.val)
+        );
+
+        // put k heads into heap
+        for (ListNode head : lists) {
+            if (head != null) {
+                pq.add(head);
+            }
+        }
+
+        while (!pq.isEmpty()) {
+            // get the minimum head from k heads
+            ListNode node = pq.poll();
+            p.next = node;
+            if (node.next != null) {
+                pq.add(node.next);
+            }
+            p = p.next;
+        }
+        return dummy.next;
+    }
 }
