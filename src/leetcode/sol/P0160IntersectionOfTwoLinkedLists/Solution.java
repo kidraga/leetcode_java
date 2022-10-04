9c18acb279f4de416d550bd37affcc5c821c83d7
Zheng Luo <kidraga@gmail.com>
2022-10-04 16:32:22 -0700

    Problem 160

---
+++ src/leetcode/sol/P0160IntersectionOfTwoLinkedLists/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.sol.P0160IntersectionOfTwoLinkedLists;
+
+import leetcode.sol.util.ListNode;
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1 != p2) {
+            // p1 goes one step. If it reaches end of list 1, go to list 2
+            if (p1 == null) {
+                p1 = headB;
+            } else {
+                p1 = p1.next;
+            }
+
+            // p2 goes one step. If it reaches end of list 2, go to list 1
+            if (p2 == null) {
+                p2 = headA;
+            } else {
+                p2 = p2.next;
+            }
+        }
+        return p1;
+    }
+}
