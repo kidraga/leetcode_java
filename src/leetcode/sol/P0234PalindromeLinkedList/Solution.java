cbb1c2bc1bdb12cd0a0851d8c0ee6d4bbec5d989
Zheng Luo <kidraga@gmail.com>
2022-10-05 16:58:48 -0700

    Problem 234

---
+++ src/leetcode/sol/P0234PalindromeLinkedList/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.sol.P0234PalindromeLinkedList;
+
+import leetcode.sol.util.ListNode;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode node = head;
+        while (node != null) {
+            stack.push(node);
+            node = node.next;
+        }
+
+        ListNode p1 = head;
+        ListNode p2 = null;
+        while (!stack.isEmpty()) {
+            p2 = stack.pop();
+            if (p1.val != p2.val) return false;
+            p1 = p1.next;
+        }
+        return true;
+    }
+}
---
+++ src/leetcode/sol/P0234PalindromeLinkedList/Solution2.java
@@ -0,0 +1,19 @@
+package leetcode.sol.P0234PalindromeLinkedList;
+
+import leetcode.sol.util.ListNode;
+
+public class Solution2 {
+    private ListNode left;
+    public boolean isPalindrome(ListNode head) {
+        left = head;
+        return traverse(head);
+    }
+
+    private boolean traverse(ListNode right) {
+        if (right == null) return true;
+        boolean res = traverse(right.next);
+        res = res && (right.val == left.val);
+        left = left.next;
+        return res;
+    }
+}
