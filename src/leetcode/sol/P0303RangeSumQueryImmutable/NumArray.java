720543feaa97139508591efbf2684f0c93e77842
Zheng Luo <kidraga@gmail.com>
2022-10-06 10:49:29 -0700

    Problem 303

---
+++ src/leetcode/sol/P0303RangeSumQueryImmutable/NumArray.java
@@ -0,0 +1,15 @@
+package leetcode.sol.P0303RangeSumQueryImmutable;
+
+public class NumArray {
+    private int[] prefixSum;
+    public NumArray(int[] nums) {
+        prefixSum = new int[nums.length + 1];
+        for (int i = 1; i < prefixSum.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return prefixSum[right + 1] - prefixSum[left];
+    }
+}
