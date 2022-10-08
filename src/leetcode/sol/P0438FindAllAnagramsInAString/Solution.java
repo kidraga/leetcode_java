457d481ea07945ce30cf1433eff025b8e8067b32
Zheng Luo <kidraga@gmail.com>
2022-10-07 17:57:13 -0700

    Problem 438

---
+++ src/leetcode/sol/P0438FindAllAnagramsInAString/Solution.java
@@ -0,0 +1,52 @@
+package leetcode.sol.P0438FindAllAnagramsInAString;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        for (char c : p.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0;
+        int right = 0;
+        int valid = 0;
+        List<Integer> res = new ArrayList<>();
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+
+            // 更新window的数据
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+
+            // 判断左侧是否需要收缩
+            while (right - left >= p.length()) {
+                // 当窗口符合条件时，把起始index放入结果里
+                if (valid == need.size()) {
+                    res.add(left);
+                }
+
+                char d = s.charAt(left);
+                left++;
+                // 更新窗口信息
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+        return res;
+    }
+}
