caf510c493e29da8bd941d08e4d969ccc39e8f23
Zheng Luo <kidraga@gmail.com>
2022-10-24 14:38:21 -0700

    Problem 230

---
+++ src/leetcode/sol/P0230KthSmallestElementInABST/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.sol.P0230KthSmallestElementInABST;
+
+import leetcode.sol.util.TreeNode;
+
+public class Solution {
+    int res = 0;
+    int rank = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        traverse(root, k);
+        return res;
+    }
+
+    private void traverse(TreeNode root, int k) {
+        if (root == null) return;
+
+        traverse(root.left, k);
+        rank++;
+        if (k == rank) {
+            res = root.val;
+            return;
+        }
+        traverse(root.right, k);
+    }
+}
