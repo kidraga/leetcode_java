d2f5e75a71af7e5f851137c8a87ca4fc994fd01e
Zheng Luo <kidraga@gmail.com>
2022-10-24 16:49:39 -0700

    Problem 538 and 1038

---
+++ src/leetcode/sol/P0538ConvertBSTToGreaterTree/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.sol.P0538ConvertBSTToGreaterTree;
+
+import leetcode.sol.util.TreeNode;
+
+public class Solution {
+    private int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    private void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.right);
+        // 维护累加和
+        sum += root.val;
+        root.val = sum;
+        traverse(root.left);
+    }
+}
---
+++ src/leetcode/sol/P1038BinarySearchTreeToGreaterSumTree/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.sol.P1038BinarySearchTreeToGreaterSumTree;
+
+import leetcode.sol.util.TreeNode;
+
+/**
+ * Same as 538
+ */
+public class Solution {
+    private int sum = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    private void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.right);
+        // 维护累加和
+        sum += root.val;
+        root.val = sum;
+        traverse(root.left);
+    }
+}
