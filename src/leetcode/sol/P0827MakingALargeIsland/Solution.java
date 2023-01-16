32e5c99d34feb8c56ade3f72918ab6bcce54ad6c
Zheng Luo <kidraga@gmail.com>
2023-01-15 17:24:08 -0800

    Added some comments for problem 827

--- src/leetcode/sol/P0827MakingALargeIsland/Solution.java
+++ src/leetcode/sol/P0827MakingALargeIsland/Solution.java
@@ -5,6 +5,12 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * 思路: 对每个岛编号,从碰见的第一个岛开始, dfs遍历岛的所有cell,并把所有cell的值都变成岛的index
+ * 同时,求出岛的面积,存到int[] area里, index就是岛的编号
+ * 完成后,重新遍历grid里所有为0的cell, 看看四个邻居的面积是多大,可以用它们的index来去重,
+ * 4个邻居的面积和+1就是当前cell变成1后能连成的最大面积. 用一个数不停的更新这个值求出整个grid的最大就行.
+ */
 class Solution {
 
     int[] dr = new int[]{-1, 0, 1, 0};
@@ -16,6 +22,9 @@ class Solution {
         this.grid = grid;
         N = grid.length;
 
+        // 每碰到一个岛,就给这个岛一个编号index
+        // 遍历的同时求出面积
+        // 因为原grid里已经用了0和1, 我们的编号就从2开始
         int index = 2;
         int[] area = new int[N*N + 2];
         for (int r = 0; r < N; r++) {
@@ -27,8 +36,11 @@ class Solution {
             }
         }
 
+        // 遍历所有的0
+        // 找出4个邻居的面积,用邻居岛的index去重
+        // 不停的更新最大面积sum(4个邻居岛面积)+1
         int ans = 0;
-        for (int x : area) ans = Math.max(ans, x);
+        for (int x : area) ans = Math.max(ans, x); // 最差的情况就是最大值是单个岛的最大面积. 为什么不是最大岛+1?因为有可能grid里没有0
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
                 if (grid[r][c] == 0) {
@@ -52,7 +64,6 @@ class Solution {
         grid[r][c] = index;
         for (Integer move : neighbors(r, c)) {
             if (grid[move / N][move % N] == 1) {
-                grid[move / N][move % N] = index;
                 ans += dfs(move / N, move % N, index);
             }
         }
